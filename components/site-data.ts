5b4bfbecb8cfe6a844e9dbb0e783e5693fd39f01
averyandrews0 <avery.mz.a@gmail.com>
2026-07-13 13:45:28 -0400

    Update site-data.ts

--- components/site-data.ts
+++ components/site-data.ts
@@ -1,7 +1,7 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export const links = {
-  resume: `$/resume.pdf`,
+  resume: `/resume.pdf`,
   email: "mailto:avery.mz.a@gmail.com",
   emailLabel: "avery.mz.a@gmail.com",
   writingAccess:
